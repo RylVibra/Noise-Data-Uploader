1be65c0fea65fc0223f3bb077888de755c493f68
RylVibra <168001434+RylVibra@users.noreply.github.com>
2025-06-11 12:56:24 -0400

    add datetime_to date. used a loop to add segmented data from weeks ago - it's inactive now. planning to run it in loop.

--- sigicom_noise_uploader.py
+++ sigicom_noise_uploader.py
@@ -5,6 +5,7 @@ import logging
 import ftplib
 import json
 import toml
+import time
 import io
 
 #### Config ####
@@ -24,9 +25,9 @@ header = {"accept":"application/json"}
 
 
 #### Logging ####
-logger_name = "sigicom_noise_uploader"
+logger_name = "sigicom_noise_uploader.log"
 logger= logging.getLogger(logger_name)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 # Create a formatter and set it for RotatingFileHandler
 handler = RotatingFileHandler(logger_name, 
                               maxBytes=1_000_000, 
@@ -47,16 +48,19 @@ def call_data_from_sigicom(data_url:str):
         auth=("user", f"{user_id}:{user_token}"),
         )
 
-def set_data_search(date:str, device_id):
+def set_data_search(date_start:datetime, date_end:datetime, device_id):
     """Get data url for device"""
+    pattern = "%Y-%m-%d %H:%M"
     search_url = sigicom_api_url + f"/api/v1/sensor/{device_id}/search"
     logger.debug(f"Requesting search url: {search_url}")
-    payload = {"datetime_from": date,
+    payload = {"datetime_from": date_start.strftime(pattern),
+               "datetime_to":date_end.strftime(pattern),
                "data_types": {
                    "transient": False,
                    "interval": True},
                 "aggregator": 1,
                 "timezone": "America/New_York"}
+    logger.debug(payload)
     return requests.post(
         url=search_url, 
         headers=header,
@@ -80,45 +84,47 @@ def upload_file(ftp: ftplib.FTP, file_name: str, data:io.BytesIO)->bool:
 
 
 def main():
-    time_offset = datetime.now() - timedelta(hours=2)
-    time_offset = time_offset.strftime("%Y-%m-%d %H:%M")
-    response = set_data_search(time_offset, device_id)
+    # start_date = datetime(2025,6,11)
+    # now = datetime.now()
+    # while start_date < now:
+    #     end_date = start_date + timedelta(days=5)
+    end_date = datetime.now().replace(second=0,microsecond=0)
+    start_date = end_date - timedelta(hours=2)
+    response = set_data_search(start_date, end_date, device_id)
     # Get data url out of search response and call it.
     search_data = json.loads(response.content.decode("utf-8"))
     response = call_data_from_sigicom(search_data["data_url"])
-
     # Decode binary to string and load JSON
-    data = json.loads(response.content.decode("utf-8"))
+    if response.content:
+        data = json.loads(response.content.decode("utf-8"))
+        with open("output.json", 'w') as file:
+            json.dump(data, file, indent=4)
+        # Create a data list to hold all the row values
+        # Dynamic headers don't work with FTP endpoint.
+        final_list = ["datetime,LAS,L90,L50,L10,LAeq"]
+        if len(data['intervals'])>2:
+            for item in data['intervals']:
+                data_list = []
+                data_list.append(item['datetime'])
+                data_point = item[f'{device_id}']['intervals'] # will be list of dict
+                for value in data_point:
+                    if 'max' in value.keys():
+                        num_val = value['max']
+                    else:
+                        num_val = value['value']
+                    data_list.append(str(round(num_val,3)))
+                joined_data = ','.join(data_list)
+                final_list.append(joined_data)
+            csv_string = '\n'.join(final_list)
 
-    # # Save to a JSON file
-    # with open("output.json", "w", encoding="utf-8") as f:
-    #     json.dump(data['intervals'], f, indent=4)
-    
-    # Create a data list to hold all the row values
-    # Dynamic headers don't work with FTP endpoint.
-    final_list = ["datetime,LAS,L90,L50,L10,LAeq"]
-    for item in data['intervals']:
-        data_list = []
-        data_list.append(item['datetime'])
-        data_point = item[f'{device_id}']['intervals'] # will be list of dict
-        for value in data_point:
-            if 'max' in value.keys():
-                num_val = value['max']
-            else:
-                num_val = value['value']
-            data_list.append(str(round(num_val,3)))
-        joined_data = ','.join(data_list)
-        final_list.append(joined_data)
-    csv_string = '\n'.join(final_list)
-
-    # Convert the CSV string to bytes
-    csv_bytes = csv_string.encode('utf-8')
-    logger.debug(csv_string)
-    filename = f"device_data.csv"
-    conn = connect_ftp(username=username)
-    result = upload_file(conn, filename, io.BytesIO(csv_bytes))
-    logger.info(result, "Finished")
-
+            # Convert the CSV string to bytes
+            csv_bytes = csv_string.encode('utf-8')
+            logger.debug(csv_string)
+            filename = f"device_data.csv"
+            conn = connect_ftp(username=username)
+            result = upload_file(conn, filename, io.BytesIO(csv_bytes))
+            logger.info(f"{result}Finished")
+            # time.sleep(10)
 
 if __name__ == "__main__":
     main()
